ee28081c2699b3f0485ab7ce52b33a76987402f5
AhmedAbo-zeid <am1597535@gmail.com>
2022-03-15 18:51:04 +0200

    Test_LED

--- 4-APP/main.c
+++ 4-APP/main.c
@@ -5,9 +5,28 @@
 /*******************************************************/
 /*******************************************************/
 
-#include "..\5-LIB\STD_TYPES.h"
+#include "STD_TYPES.h"
+
+#include "DIO_interface.h"
+#include "PORT_interface.h"
+#include "LED_interface.h"
 
 void main(void)
 {
+	PORT_voidInit();
 	
+	typedef struct My_Struct1
+	{
+		LED_PORTA;
+		LED_PIN0;
+		LED_u8ACTIVE_HIGH;
+	}LED_1;
+	
+	while(1)
+	{
+		if(DIO_u8GetPinValue(DIO_u8PORTA ,DIO_u8PIN1)==1)
+		{
+			LED_voidLedToggle(&LED_1);
+		}
+	}
 }
